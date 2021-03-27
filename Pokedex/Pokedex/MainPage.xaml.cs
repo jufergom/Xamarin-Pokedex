bf09efa8d4f5c16b35812e5aae8bf7903b5058b8
Julio Gomez <jufergom@hotmail.com>
2021-03-27 10:29:01 -0600

    created a new listview page and added navigation between pages

--- Pokedex/Pokedex/MainPage.xaml.cs
+++ Pokedex/Pokedex/MainPage.xaml.cs
@@ -15,9 +15,9 @@ namespace Pokedex
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-
+            await Navigation.PushAsync(new PokemonList());
         }
     }
 }
